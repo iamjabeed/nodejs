cbea439ce5329cf825c611787d60fbd3fa0a0c9b
iamjabeed <smartyjabeedbhai001@gmail.com>
2023-06-03 21:05:26 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const names = ["jaddu", "james"];
+const ages = [69, 33];
+// console.log(names);
+
+function sum(a, b) {
+  return a + b;
+}
+
+module.exports = {
+  names,
+  ages,
+  sum,
+};
---
+++ index.js
@@ -0,0 +1,22 @@
+// console.log("Hello NodeJS");
+
+//* Global object
+// console.log(global);
+
+// global.setTimeout(()=>{
+//     console.log("Hello NodeJS");
+// },1000);
+
+// console.log(__dirname)
+// console.log(__filename)
+
+//* Module
+const names = require("./app");
+
+console.log(names.sum(2,3));
+
+
+// function sum(a, b) {
+//   console.log(a + b);
+// }
+// sum(2, 3);
