dfa8a33884dfd777230ff9e12d0b69b99f4d7236
iamjabeed <smartyjabeedbhai001@gmail.com>
2023-06-05 15:38:22 +0530

    req and response

--- server.js
+++ server.js
@@ -2,6 +2,15 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   console.log("request made");
+
+  //* Step-1: set header
+  res.setHeader("Content-Type", "text/html");
+
+  //* Step-2: send response
+  res.write("<h1>Hello from node server</h1>");
+
+  //* Step-3: end server
+  res.end();
 });
 
 server.listen(3300, "localhost", () => {
