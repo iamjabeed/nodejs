25f0bc6addea61d13f6c2dc5b3992ad3b6fd059e
iamjabeed <smartyjabeedbhai001@gmail.com>
2023-06-06 17:20:32 +0530

    basic routing

--- express/app.js
+++ express/app.js
@@ -2,10 +2,21 @@ const express = require("express");
 const app = express();
 // console.log("nodemon");
 const port = 3000
+
+
 app.get("/", (req, res) => {
   console.log("listening on port 3000");
-  res.send('Hello World!')
+  res.send('<h1>Hello World!</h1>')
 });
+app.get("/about", (req, res) => {
+  
+  res.send("<p>nodemon supports local and global configuration files. These are usually named nodemon.json and can be located in the current working directory or in your home directory.</p>")
+});
+app.get("/contact", (req, res) => {
+  
+  res.send('Contact username')
+});
+
 
 app.listen(port, (req, res) => {
     console.log(`Example app listening on port ${port}`)
